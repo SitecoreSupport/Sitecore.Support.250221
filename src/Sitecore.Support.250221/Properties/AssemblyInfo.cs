d95b06e926e96a4c1f5d1909d2822ece5bb5b599
Anton Kechashin <atk@sitecore.net>
2019-10-04 16:27:50 +0300

    Fix for SXA 1.8.0

---
+++ src/Sitecore.Support.250221/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.250221")]
+[assembly: AssemblyProduct("Sitecore.Support.250221")]
+[assembly: ComVisible(false)]
